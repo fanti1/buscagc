4072ae4794eb26823e06c6bc5f1c2baaa1fed5dc
fanti1 <nicolasfanty@gmail.com>
2020-10-13 21:19:11 -0300

    cu

--- app.py
+++ app.py
@@ -1,29 +1,41 @@
 from flask import Flask, render_template, request, redirect
-import requests, sys, re, os
+import requests
+import sys
+import re
+import os
 from bs4 import BeautifulSoup
 from json import JSONDecoder, dumps, load
 from functools import partial
 import concurrent.futures
 from OpenSSL import SSL
-
 app = Flask(__name__)
-
 steamapikey = "FDBB490D0187D0AA68E36B5C28CC2657"
-
 session = requests.session()
 jar = requests.cookies.RequestsCookieJar()
 jar.set('gclubsess', '52a532c1fc2b17cab854eb25e7aac1ea95d3e2c3')  # 08.10.20
 session.cookies = jar
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
+
+
+@app.errorhandler(500)
+def internal_error(e):
+    return render_template("500.html")
+
+
 @app.route('/', methods=['POST', 'GET'])
 def busca():
     if request.method == 'GET':
-        stats = read_hash('buscas_recentes')
-
-        if not stats:
+        
+        stats = read_hash('buscas_recentes')       
+        if len(stats) == 0:
+            print('rendewrizou fallen')
             stats_fallen = get_stats(94)
             return render_template("fallen.html", player_stats=stats_fallen)
         else:
+            print('rendewrizou recente')
             return render_template("recents.html", players=stats)
 
     if request.method == 'POST':
@@ -35,18 +47,7 @@ def busca():
         steamid64 = get_profile(url_front.split("/")[4])
         return render_template("index.html", player=dados_player, erro_player=erroPlayer, isAdmin=isAdmin, steam64orsteamid=steamid64, player_stats=player_stats)
 
-def grab_file_time(hashid):
-    import datetime
-    today = datetime.datetime.today()
-    modified_date = datetime.datetime.fromtimestamp(os.path.getctime(f'cache/{hashid}.txt'))
-    duration = today - modified_date
 
-    if duration.seconds > 7200:
-        return True
-    else:
-        return False
-
-# nao deixando o player entrar na url /partida
 @app.route('/partida', methods=['GET'])
 def red():
     return busca()
@@ -80,7 +81,7 @@ def fuckoff(steamid):
 def search_mult():
     if request.method == 'POST':
 
-        #remove os files com mais de 2 horas menos o buscas_recentes
+        # remove os files com mais de 2 horas menos o buscas_recentes
         get_old_files("cache/")
 
         url_front = request.form['content']
@@ -88,8 +89,22 @@ def search_mult():
         return render_template("index2.html", players=grab_players, erro_player=erroPlayer, isAdmin=isAdmin, steam64orsteamid=hashkey)
 
 
+def grab_file_time(hashid):
+    import datetime
+    today = datetime.datetime.today()
+    modified_date = datetime.datetime.fromtimestamp(
+        os.path.getctime(f'cache/{hashid}.txt'))
+    duration = today - modified_date
+
+    if duration.seconds > 7200:
+        return True
+    else:
+        return False
+
+
 def get_old_files(path):
-    import time, os
+    import time
+    import os
     now = time.time()
 
     for filename in os.listdir(path):
@@ -99,54 +114,46 @@ def get_old_files(path):
             if not 'buscas_recentes.txt' in filename:
                 os.remove(f"cache/{filename}")
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template("404.html")
-
-
-@app.errorhandler(500)
-def internal_error(e):
-    return render_template("500.html")
 
 def get_hash():
-    import random, string
+    import random
+    import string
 
     letters_and_digits = string.ascii_letters.lower() + string.digits
     result_str = ''.join((random.choice(letters_and_digits) for i in range(5)))
 
     return result_str
 
-def isInt(n):
-    try:
-        int(n)
-        return True
-    except ValueError:
-        return False
-
 # usado no read_hash
+
+
 def json_parse(fileobj, decoder=JSONDecoder(), buffersize=2048):
     buffer = ''
     for chunk in iter(partial(fileobj.read, buffersize), ''):
-         buffer += chunk
-         while buffer:
-             try:
-                 result, index = decoder.raw_decode(buffer)
-                 yield result
-                 buffer = buffer[index:].lstrip()
-             except ValueError:
-                 # Not enough data to decode, read more
-                 break
+        buffer += chunk
+        while buffer:
+            try:
+                result, index = decoder.raw_decode(buffer)
+                yield result
+                buffer = buffer[index:].lstrip()
+            except ValueError:
+                # Not enough data to decode, read more
+                break
 
-def read_hash( hashid, defaut = 0 ):
+
+def read_hash(hashid, defaut=0):
     if defaut == 1:
-        with open( f'cache/{hashid}.txt') as f:
+        with open(f'cache/{hashid}.txt') as f:
             return load(f)
     else:
         return list(json_parse(open(f'cache/{hashid}.txt')))
 
+
 def get_profile(steamid):
+
     sixtyfourid = None
-    if not isInt(steamid) or len(str(steamid)) != 17:
+
+    if len(str(steamid)) != 17:
         vanitynameurl = "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key={}&vanityurl={}"
         url = vanitynameurl.format(steamapikey, steamid)
         r = requests.get(url)
@@ -172,6 +179,7 @@ def steamid_to_64bit(steamid):
         steam64id += 1
     return steam64id
 
+
 def get_multi_profiles(text):
     p = re.compile(r'\STEAM_[0-5]:[01]:\d+')
     steamids = p.findall(text)
@@ -182,20 +190,21 @@ def get_multi_profiles(text):
         l_steamid.append(steamid_to_64bit(steamid))
 
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        future_to_url = {executor.submit(consulta_url, url, "True"): url for url in l_steamid}
+        future_to_url = {executor.submit(
+            consulta_url, url, "True"): url for url in l_steamid}
         for future in concurrent.futures.as_completed(future_to_url):
             url = future_to_url[future]
             try:
                 players.append(future.result())
             except Exception as exc:
                 print('%r generated an exception: %s' % (url, exc))
-        
+
         global hashkey
         hashkey = get_hash()
         with open(f'cache/{hashkey}.txt', 'w') as json_file:
             json_file.write(dumps(players))
             json_file.close()
-    
+
         return players
 
 
@@ -211,38 +220,39 @@ def getAdmin(url):
 def get_stats(userid):
     stats = session.get(f'https://gamersclub.com.br/api/box/history/{userid}')
     player_s = []
-
     if 'stat' in stats.json():
         for entry in stats.json()['stat']:
             case = {'stat': entry['stat'], 'value': entry['value']}
             player_s.append(case)
     else:
         player_s.append("error")
-
     return player_s
 
 
-def consulta_url(profile_url, steamids = 'False'):
+def consulta_url(profile_url, steamids='False'):
     global isAdmin
     global erroPlayer
     global player_stats
     player = {}
 
     if 'True' in steamids:
-        page = session.get( f'https://gamersclub.com.br/buscar?busca=http://steamcommunity.com/profiles/{profile_url}', stream = True )
+        page = session.get(
+            f'https://gamersclub.com.br/buscar?busca=http://steamcommunity.com/profiles/{profile_url}', stream=True)
         isAdmin = False
     else:
-        page = session.get( f'https://gamersclub.com.br/buscar?busca={profile_url}' )
+        page = session.get(
+            f'https://gamersclub.com.br/buscar?busca={profile_url}')
         isAdmin = getAdmin(profile_url)
 
     soup = BeautifulSoup(page.text, "html.parser")
 
     # a classe jumbotron só aparece quando o jogador nao tem conta.
-    has_account = soup.find(class_='jumbotron')
-    if has_account:
+    no_account = soup.find(class_='jumbotron')
+    if no_account:
         erroPlayer = True
-
-        items = [item.next_sibling for item in soup.select(".jumbotron p strong")]
+        player_stats = False
+        items = [item.next_sibling for item in soup.select(
+            ".jumbotron p strong")]
 
         player[u'name'] = items[0]
         player[u'steamid'] = items[1]
@@ -261,12 +271,9 @@ def consulta_url(profile_url, steamids = 'False'):
     else:
         erroPlayer = False
 
-        userid = soup.find('div', 'gc-profile-user-id').get_text().split(": ")[1]
+        userid = soup.find(
+            'div', 'gc-profile-user-id').get_text().split(": ")[1]
         player[u'id'] = userid
-
-        if 'False' in steamids:
-            player_stats = get_stats(userid)
-
         name = soup.find('div', 'gc-profile-user-container').get_text()
         player[u'name'] = name
 
@@ -279,9 +286,10 @@ def consulta_url(profile_url, steamids = 'False'):
             lvl = class_box.find_all("div", class_="gc-featured-item")[2:3]
             player[u'lvl'] = lvl[0].get_text().split()[0]
 
-        image = soup.find('div', {"class": "gc-profile-avatar-img-container"}).img['src']
+        image = soup.find(
+            'div', {"class": "gc-profile-avatar-img-container"}).img['src']
         player[u'avatar'] = image
-    
+
         country = soup.find("span", {"class": "gc-profile-user-flag"})
         player[u'country'] = country['title']
         player[u'flag'] = country.img['src'][-6:]
@@ -300,7 +308,8 @@ def consulta_url(profile_url, steamids = 'False'):
 
                     show_ban = True
                 else:
-                    reason = soup.find("span", class_="primary-color").get_text()
+                    reason = soup.find(
+                        "span", class_="primary-color").get_text()
                     player[u'reason'] = reason
 
                     if reason:
@@ -325,7 +334,7 @@ def consulta_url(profile_url, steamids = 'False'):
         player[u'isBanned'] = is_user_banned
 
         # nao quero que salve os players buscados no multi search
-        if 'False' in steamids: 
+        if 'False' in steamids:
             player_stats = get_stats(userid)
             player['p_stats'] = list(player_stats)
 
@@ -334,19 +343,20 @@ def consulta_url(profile_url, steamids = 'False'):
             qwert = re.search(f"{player['steam']}", steamchecker)
 
             if not qwert:
-                #verificando o tamanho do arquivo se maior q 10mb limpa.
+                # verificando o tamanho do arquivo se maior q 10mb limpa.
                 statinfo = os.stat(filename)
                 if statinfo.st_size >= 1048576:
-                    file = open(filename,"r+")
+                    file = open(filename, "r+")
                     file.truncate(0)
                     file.close()
 
                 with open(filename, 'a') as json_file:
-                	if is_user_banned == False:
-	                    json_file.write(dumps(player))
-	                    json_file.close()
-    
+                    
+                    json_file.write(dumps(player))
+                    json_file.close()
+
         return player
+
 
 if __name__ == '__main__':
     # tratativa pra executar run certo se tiver no linux ou se tiver no windows testando
